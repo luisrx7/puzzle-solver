bb6259dfadb2f0951818cb3fe2bac03f413f6d8c
Todd Lewandowski <todd.lewandowski@gmail.com>
2022-10-09 15:05:00 -0700

    Ready to test

--- puzzle-gui.py
+++ puzzle-gui.py
@@ -6,14 +6,11 @@ import tkinter.filedialog
 import math
 import myvideocapture as mvc
 
-PADDING_X = 15
-PADDING_Y = 10
-IMAGE_WIDTH = 600
-IMAGE_HEIGHT = 335
-CAMERA_WIDTH = 533
-CAMERA_HEIGHT = 400
+CAMERA_WIDTH = 563
+CAMERA_HEIGHT = 450
 MIN_MATCH_COUNT = 4
-MINION_DIMENSIONS = (3, 4)
+ICECREAMTRUCK_DIMENSIONS = (20,15)
+CROP_FACTOR = 0.2
 
 class puzzleGUI:
 
@@ -21,6 +18,8 @@ class puzzleGUI:
         self.master = master
         self.query_image = None
         self.train_image = None
+        self.kp2 = None
+        self.des2 = None
 
         # Open video source (by default this will try to open the computer webcam)
         self.video_source = video_source
@@ -38,7 +37,7 @@ class puzzleGUI:
     def setup_gui (self, master):
         # Create GUI elements
         self.recent_label = tk.Label(master, text='Most Recent Piece')
-        self.analysis_label = tk.Label(master, text='Target & Analysis')
+        self.analysis_label = tk.Label(master, text='Target Image')
         self.recent_image = tk.Label(master, text=' ')
         self.analysis_image = tk.Label(master, text=' ')
         self.getfile_button = tk.Button(master, text='Import', command=self.get_piece_from_file)
@@ -68,19 +67,33 @@ class puzzleGUI:
         self.capture_button.grid(row=7, column=0)
         self.quit_button.grid(row=7, column=1)
 
+    def crop_image (self, image):
+        factor = CROP_FACTOR
+        (width, height, color) = image.shape
+        return image[:,int(factor*height):int(height*(1-factor))]
+
     def snapshot(self):
         # Get a frame from the video source
         ret, frame = self.vid.get_frame()
+        frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
+        frame = self.crop_image(frame)
+
         if ret:
-            photo = ImageTk.PhotoImage(image = Image.fromarray(frame).resize((CAMERA_WIDTH, CAMERA_HEIGHT)))
+            photo = ImageTk.PhotoImage(image = Image.fromarray(frame).resize((int(CAMERA_WIDTH*(1-2*CROP_FACTOR)), CAMERA_HEIGHT)))
             self.recent_image.configure(image=photo)
             self.recent_image.photo = photo
+            self.query_image = frame
+            self.analyze_piece()
 
     def get_poster_image(self):
         pil_photo, cv_photo = self.get_image_from_file()
         self.analysis_image.configure(image=pil_photo)
         self.analysis_image.photo = pil_photo
         self.train_image = cv_photo
+
+        sift = cv2.SIFT_create()
+        kp2, des2 = sift.detectAndCompute(self.train_image, None)
+        # Get features of target image and  analyze them at load rather than with every new piece
 
     def get_piece_from_file(self):
         pil_photo, cv_photo = self.get_image_from_file()
@@ -97,7 +110,7 @@ class puzzleGUI:
         resized = load.resize((CAMERA_WIDTH, CAMERA_HEIGHT))
         pil_photo = ImageTk.PhotoImage(resized)
 
-        cv_photo = cv2.imread(name,cv2.IMREAD_GRAYSCALE)
+        cv_photo = cv2.imread(name)
 
         return pil_photo, cv_photo
 
@@ -122,7 +135,8 @@ class puzzleGUI:
         if k > len(points):
             k = len(points)  # If less matches than 4
         criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-        compactness, label, center = cv2.kmeans(points, k, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+        if len(points) > 1:
+            compactness, label, center = cv2.kmeans(points, k, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
 
         # Separate the data into clusters, find cluster with most elements, and get its centroid
         series = []
@@ -143,8 +157,9 @@ class puzzleGUI:
         self.matches_label.configure(text="Matches: "+str(len(good)))
 
         # Rotate the piece back to the correct orientation
-        img4 = self.train_image  # Copy of global image for feature-matching
-        rows, cols = self.query_image.shape
+        img4 = self.train_image.copy()  # Copy of global image for feature-matching
+        try: rows, cols, colors = self.query_image.shape
+        except: rows, cols = self.query_image.shape # File input has 2 values, webcam has 3
         if len(good) >= MIN_MATCH_COUNT:
             src_pts = np.float32([kp1[m.queryIdx].pt for m in good ]).reshape(-1, 1, 2)
             dst_pts = np.float32([kp2[m.trainIdx].pt for m in good ]).reshape(-1, 1, 2)
@@ -154,37 +169,43 @@ class puzzleGUI:
             # Estimate an affine transform
             Mprime = cv2.estimateAffinePartial2D(src_pts, dst_pts)
             degrees = math.degrees(math.atan2(Mprime[0][1][0], Mprime[0][0][0])) * (-1.0)
-            self.rotation_label.configure(text="Rotation: " + str(round(degrees)))
+            text = str(round(abs(degrees)))
+            if degrees > 0:
+                text += " counterclockwise"
+            else:
+                text += " clockwise"
+            self.rotation_label.configure(text="Rotation: " + text)
 
             # Rotate the piece back
             M = cv2.getRotationMatrix2D((cols/2,rows/2), degrees, 1)
             img5 = self.query_image.copy()
             img5 = cv2.warpAffine(self.query_image, M, (cols, rows))
-        # else:
-            # # print( "Not enough matches are found - {}/{}".format(len(good), MIN_MATCH_COUNT) )
-            # matchesMask = None
-            # img5 = Piece.rotate_piece(self.query_image, puzzleName, pieceName)
+        else:
+            # print( "Not enough matches are found - {}/{}".format(len(good), MIN_MATCH_COUNT) )
+            matchesMask = None
+
         draw_params = dict(matchColor = (0, 255, 0),
            singlePointColor = None,
            matchesMask = matchesMask,  # Only draw the inliers
            flags = 2)
 
-        img4 = cv2.drawMatches(self.query_image, kp1, img4, kp2, good, img4, **draw_params)
-
         # Find the centroid of the cluster
-        (ydim, xdim) = self.train_image.shape
-        (xx, yy) = MINION_DIMENSIONS # DORY_DIMENSIONS if 'dory' in puzzleName else MINION_DIMENSIONS
+        (ydim, xdim, colors) = self.train_image.shape
+        (xx, yy) = ICECREAMTRUCK_DIMENSIONS # DORY_DIMENSIONS if 'dory' in puzzleName else MINION_DIMENSIONS
         (xdim, ydim) = (xdim/xx, ydim/yy)
         (xcoord, ycoord) = (int(math.floor(x/xdim)), int(math.floor(y/ydim)))
         self.location_label.configure(text="Location: ("+str(xcoord+1)+", "+str(ycoord+1)+")")  # Start from 0 is unnatural
         x = int(math.floor(x/xdim)*xdim + xdim/2)  # Discretize the centroid
         y = int(math.floor(y/ydim)*ydim + ydim/2)
-        rows, cols = self.query_image.shape
-        x += cols  # Offset centroid to account for the side-by-side image
+
+        try: rows, cols, colors = self.query_image.shape
+        except: rows, cols = self.query_image.shape # File input has 2 values, webcam has 3
+
         cv2.circle(img4, (x,y), 150, 255, -15)  # Draw centroid in blue for this specific piece output
 
         # Resize and show on screen
-        (x, y, c) = img4.shape
+        try: (x, y, c) = img4.shape
+        except: (x, y) = img4.shape
         (newx, newy) = (int(math.floor(y*CAMERA_HEIGHT*1.0/x)), CAMERA_HEIGHT)
         resized = cv2.resize(img4, (newx, newy))
         img = cv2.cvtColor(resized, cv2.COLOR_BGR2RGB)
@@ -196,7 +217,9 @@ class puzzleGUI:
         # Get a frame from the video source
         ret, frame = self.vid.get_frame()
         if ret:
-            photo = ImageTk.PhotoImage(image = Image.fromarray(frame).resize((CAMERA_WIDTH, CAMERA_HEIGHT)))
+            frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
+            frame = self.crop_image(frame)
+            photo = ImageTk.PhotoImage(image = Image.fromarray(frame).resize((int(CAMERA_WIDTH*0.6), CAMERA_HEIGHT)))
             self.camera_image.configure(image=photo)
             self.camera_image.photo = photo
         self.master.after(self.delay, self.update)
